267e5b04d1e3ef2d5808ea00ddb9115aeff81631
Nadav Rotem <nadav256@gmail.com>
2023-09-27 12:52:13 -0700

    Sketch a simple DMC model.

--- src/coding/adaptive.rs
+++ src/coding/adaptive.rs
@@ -1,6 +1,8 @@
 //! This module implements an arithmetic code such as the one in zpaq, and
 //! described by Matt Mahoney: https://mattmahoney.net/dc/dce.html#Section_32
 //! and in the book Managing Gigabytes by Witten, Moffat and Bell, section 2.4.
+use rand_distr::num_traits::Zero;
+
 use crate::utils::signatures::{match_signature, ARITH_SIG};
 use crate::utils::signatures::{read32, write32};
 use crate::utils::RECIPROCAL_U32;
@@ -293,6 +295,150 @@ fn test_simple_model() {
         let pred = model.predict(0);
         assert!(pred > 65_000);
     }
+}
+
+/// Start with context of 4 bits.
+const DMC_LEVELS: usize = 4;
+
+pub struct DMCModel {
+    state: usize,
+    /// Maps the current state to the next (0, 1) states.
+    states: Vec<[usize; 2]>,
+    /// Records the counts (events seen) for each edge.
+    counts: Vec<[f32; 2]>,
+}
+
+impl DMCModel {
+    pub fn new(levels: usize) -> Self {
+        let mut model = DMCModel {
+            state: 0,
+            states: Vec::new(),
+            counts: Vec::new(),
+        };
+        model.init(levels);
+        model
+    }
+
+    /// Create the initial state machine that has a tree-structure with 'layers'
+    fn init(&mut self, layers: usize) {
+        assert_eq!(self.states.len(), 0);
+        assert_eq!(self.counts.len(), 0);
+        assert_eq!(self.state, 0);
+        let _ = self.allocate_new_state([0, 0], [0.1, 0.1]);
+        for layer in 1..layers {
+            let len = (1 << layer) - 1;
+            for _ in 0..len {
+                let left = self.allocate_new_state([0, 0], [0.1, 0.1]);
+                let right = self.allocate_new_state([0, 0], [0.1, 0.1]);
+                self.states[left / 2][0] = left;
+                self.states[left / 2][1] = right;
+            }
+        }
+    }
+
+    /// Return a probability prediction in the 16-bit range.
+    pub fn predict(&self) -> u16 {
+        self.verify();
+        let counts = self.counts[self.state];
+        let a = counts[0];
+        let b = counts[0] + counts[1];
+        assert!(!b.is_nan());
+        assert!(!b.is_zero());
+        ((a / b) * 65536.) as u16
+    }
+
+    /// Allocate a new state and return it's index.
+    fn allocate_new_state(
+        &mut self,
+        next: [usize; 2],
+        counts: [f32; 2],
+    ) -> usize {
+        self.states.push(next);
+        self.counts.push(counts);
+        self.counts.len() - 1
+    }
+
+    fn verify(&self) {
+        let len = self.counts.len();
+        for i in 0..len {
+            let t0 = self.counts[i][0];
+            let t1 = self.counts[i][1];
+            assert!(!t0.is_zero() && !t0.is_nan());
+            assert!(!t1.is_zero() && !t1.is_nan());
+            assert!(self.states[i][0] < len && self.states[i][1] < len);
+        }
+    }
+
+    pub fn try_clone(&mut self, edge: usize) {
+        let curr = self.state;
+        let from = curr;
+        let to = self.states[curr][edge];
+
+        // This is the cost of the edge that we want to redirect.
+        let edge_count = self.counts[from][edge];
+        let sum = self.counts[to][0] + self.counts[to][1];
+
+        // Early exit good edges.
+        if edge_count < 2. || sum <= 2. + edge_count {
+            return;
+        }
+
+        assert!(edge_count != 0.);
+        assert!(sum != 0.);
+        assert!(edge_count != sum);
+
+        // Create a new node.
+        let tc = self.counts[to];
+        let r = edge_count / sum;
+        assert!(r != 1.0);
+        let tc0 = tc[0] * r;
+        let tc1 = tc[1] * r;
+        self.counts[to][0] -= tc0;
+        self.counts[to][1] -= tc1;
+        let new = self.allocate_new_state(self.states[to], [tc0, tc1]);
+        // Register the new node.
+        self.states[curr][edge] = new;
+    }
+
+    /// Update the probability of the model with the bit 'bit'.
+    /// Advance to the next state, and update the counts.
+    pub fn update(&mut self, bit: u8) {
+        self.try_clone(bit as usize);
+        self.counts[self.state][bit as usize] += 1.;
+        self.state = self.states[self.state][bit as usize];
+        self.verify();
+    }
+
+    /// Print a dotty graph of the state machine.
+    pub fn dump(&self) {
+        println!("digraph finite_state_machine {{");
+        println!("rankdir=LR;");
+        println!("node [shape = circle];");
+        for i in 0..self.counts.len() {
+            let tos = self.states[i];
+            let counts = self.counts[i];
+            println!("{} -> {} [label = \"0: {}\"];", i, tos[0], counts[0]);
+            println!("{} -> {} [label = \"1: {}\"];", i, tos[1], counts[1]);
+        }
+        println!("}}");
+    }
+}
+
+#[test]
+fn test_dmc_dump() {
+    let text = "this is a message. this is"; // a message.  this is a message.";
+    let text = text.as_bytes();
+    let mut model = DMCModel::new(DMC_LEVELS);
+
+    for b in text {
+        for i in 0..8 {
+            let bit = (b >> i) & 1;
+            let p = model.predict();
+            model.update(bit);
+            println!("pred = {}", p);
+        }
+    }
+    model.dump();
 }
 
 /// Adaptive Arithmetic Encoder. The encoder predicts the probability of the
